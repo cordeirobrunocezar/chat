1e22acf32ea20a4ec29067bf79eb257217306947
cezar <cordeirobrunocezar@outlook.com>
2024-12-09 21:12:11 -0300

    handler

--- client.py
+++ client.py
@@ -10,9 +10,10 @@ import concurrent.futures
 
 logger = logging.getLogger(__name__)
 
-addr = ""
-host = "localhost"
-port = 65431
+HOST = "localhost"
+PORT = 65431
+
+handler = None
 
 @dataclass
 class User:
@@ -33,7 +34,7 @@ class App():
 
         self.txt_chat = Text(takefocus=0,)
         self.txt_chat.pack()
-        self.txt_chat.configure(state=DISABLED)
+        self.txt_chat.configure(wrap=WORD, state=DISABLED)
 
         self.ent = Entry()
         self.ent.pack(fill=X)
@@ -84,26 +85,26 @@ class App():
         if len(idxs) == 1:
             idx = int(idxs[0])
             r = self.rooms[idx]
-            if _join_room(self.user, r):
+            if handler._join_room(self.user, r):
                 self.user.current_room = r
 
     def _new(self):
         self._dialog()
-        _create_room(self.buf)
+        handler._create_room(self.buf)
 
     def _send(self, event):
         idxs = self.lstbox_usrs.curselection()
         if len(idxs) == 1:
             idx = int(idxs[0])
             u = self.users[idx]
-            _send_message(self.user, self.ent.get(), u)
+            handler._send_message(self.user, self.ent.get(), u)
         else:
-            _send_message(self.user, self.ent.get())
+            handler._send_message(self.user, self.ent.get())
         self.ent.delete(0, END)
         self.lstbox_usrs.selection_clear(1, END)
 
     def _receive(self):
-        messages = _receive_messages(self.user)
+        messages = handler._receive_messages(self.user)
         if len(messages) > 0:
             self.txt_chat.configure(state=NORMAL)
             self.txt_chat.delete(1.0, END)
@@ -112,21 +113,21 @@ class App():
             self.txt_chat.configure(state=DISABLED)
     
     def _retrieve_rooms(self):
-        self.rooms = _list_rooms()
+        self.rooms = handler._list_rooms()
         self.rooms_var.set(self.rooms)
 
     def _retrieve_users(self):
-        self.users = _list_users(self.user)
+        self.users = handler._list_users(self.user)
         self.lst_usrs_var.set(self.users)
 
     def _login(self):
         self._dialog()
-        return _register_user(self.buf)
+        return handler._register_user(self.buf)
     
     def _update_clock(self):
         current_time = time.ctime()
         self.lbl_clock.configure(
-            text=self.user.current_room+' '+current_time
+            text=f"({self.user.name}) [{self.user.current_room}] {current_time}"
             )
         self.root.after(1000, self._update_clock)
 
@@ -159,110 +160,115 @@ class App():
         self.dlg.wait_window()
     
     def _close(self):
-        _disconnect(self.user)
+        handler._disconnect(self.user)
         self.root.grab_release()
         self.root.destroy()
 
-def _register_user(username):
-    try:
-        with xmlrpc.client.ServerProxy(f"http://{addr}") as proxy:
-            return proxy.register_user(username)
-    except Exception as e:
-        logger.error(f"{datetime.datetime.now()} at {_register_user.__name__}", exc_info=e)
-        messagebox.showerror(message=e)
-        exit()
+class Handler():
+    def __init__(self, addr):
+        self.addr = addr
+    
+    def _register_user(self, username):
+        try:
+            with xmlrpc.client.ServerProxy(f"http://{self.addr}") as proxy:
+                return proxy.register_user(username)
+        except Exception as e:
+            logger.error(f"{datetime.datetime.now()} at {self._register_user.__name__}", exc_info=e)
+            messagebox.showerror(message=e)
+            exit()
 
-def _receive_messages(usr):
-    try:
-        with xmlrpc.client.ServerProxy(f"http://{addr}", allow_none=True) as proxy:
-            return proxy.receive_messages(usr.name, usr.current_room)
-    except Exception as e:
-        logger.error(f"{datetime.datetime.now()} at {_receive_messages.__name__}", exc_info=e)
-        messagebox.showerror(message=e)
-        exit()
+    def _receive_messages(self, usr):
+        try:
+            with xmlrpc.client.ServerProxy(f"http://{self.addr}", allow_none=True) as proxy:
+                return proxy.receive_messages(usr.name, usr.current_room)
+        except Exception as e:
+            logger.error(f"{datetime.datetime.now()} at {self._receive_messages.__name__}", exc_info=e)
+            messagebox.showerror(message=e)
+            exit()
 
-def _send_message(usr, message, recipient=None):
-    try:
-        with xmlrpc.client.ServerProxy(f"http://{addr}", allow_none=True) as proxy:
-            return proxy.send_message(usr.name, usr.current_room, message, recipient)
-    except Exception as e:
-        logger.error(f"{datetime.datetime.now()} at {_send_message.__name__}", exc_info=e)
-        messagebox.showerror(message=e)
-        exit()
+    def _send_message(self, usr, message, recipient=None):
+        try:
+            with xmlrpc.client.ServerProxy(f"http://{self.addr}", allow_none=True) as proxy:
+                return proxy.send_message(usr.name, usr.current_room, message, recipient)
+        except Exception as e:
+            logger.error(f"{datetime.datetime.now()} at {self._send_message.__name__}", exc_info=e)
+            messagebox.showerror(message=e)
+            exit()
 
-def _create_room(room_name):
-    try:
-        with xmlrpc.client.ServerProxy(f"http://{addr}") as proxy:
-            return proxy.create_room(room_name)
-    except Exception as e:
-        logger.error(f"{datetime.datetime.now()} at {_create_room.__name__}", exc_info=e)
-        messagebox.showerror(message=e)
-        exit()
+    def _create_room(self, room_name):
+        try:
+            with xmlrpc.client.ServerProxy(f"http://{self.addr}") as proxy:
+                return proxy.create_room(room_name)
+        except Exception as e:
+            logger.error(f"{datetime.datetime.now()} at {self._create_room.__name__}", exc_info=e)
+            messagebox.showerror(message=e)
+            exit()
 
-def _join_room(usr, room_name):
-    _leave_room(usr)
-    try:
-        with xmlrpc.client.ServerProxy(f"http://{addr}") as proxy:
-            answer = proxy.join_room(usr.name, room_name)
-            return answer
-    except Exception as e:
-        logger.error(f"{datetime.datetime.now()} at {_join_room.__name__}", exc_info=e)
-        messagebox.showerror(message=e)
-        exit()
+    def _join_room(self, usr, room_name):
+        self._leave_room(usr)
+        try:
+            with xmlrpc.client.ServerProxy(f"http://{self.addr}") as proxy:
+                answer = proxy.join_room(usr.name, room_name)
+                return answer
+        except Exception as e:
+            logger.error(f"{datetime.datetime.now()} at {self._join_room.__name__}", exc_info=e)
+            messagebox.showerror(message=e)
+            exit()
 
-def _leave_room(usr):
-    try:
-        with xmlrpc.client.ServerProxy(f"http://{addr}") as proxy:
-            return proxy.leave_room(usr.name, usr.current_room)
-    except Exception as e:
-        logger.error(f"{datetime.datetime.now()} at {_leave_room.__name__}", exc_info=e)
-        messagebox.showerror(message=e)
-        exit()
+    def _leave_room(self, usr):
+        try:
+            with xmlrpc.client.ServerProxy(f"http://{self.addr}") as proxy:
+                return proxy.leave_room(usr.name, usr.current_room)
+        except Exception as e:
+            logger.error(f"{datetime.datetime.now()} at {self._leave_room.__name__}", exc_info=e)
+            messagebox.showerror(message=e)
+            exit()
 
-def _disconnect(usr):
-    try:
-        with xmlrpc.client.ServerProxy(f"http://{addr}") as proxy:
-            return proxy.disconnect(usr.name, usr.current_room)
-    except Exception as e:
-        logger.error(f"{datetime.datetime.now()} at {_disconnect.__name__}", exc_info=e)
-        messagebox.showerror(message=e)
-        exit()
+    def _disconnect(self, usr):
+        try:
+            with xmlrpc.client.ServerProxy(f"http://{self.addr}") as proxy:
+                return proxy.disconnect(usr.name, usr.current_room)
+        except Exception as e:
+            logger.error(f"{datetime.datetime.now()} at {self._disconnect.__name__}", exc_info=e)
+            messagebox.showerror(message=e)
+            exit()
 
-def _list_rooms():
-    try:
-        with xmlrpc.client.ServerProxy(f"http://{addr}") as proxy:
-            return proxy.list_rooms()
-    except Exception as e:
-        logger.error(f"{datetime.datetime.now()} at {_list_rooms.__name__}", exc_info=e)
-        messagebox.showerror(message=e)
-        exit()
+    def _list_rooms(self):
+        try:
+            with xmlrpc.client.ServerProxy(f"http://{self.addr}") as proxy:
+                return proxy.list_rooms()
+        except Exception as e:
+            logger.error(f"{datetime.datetime.now()} at {self._list_rooms.__name__}", exc_info=e)
+            messagebox.showerror(message=e)
+            exit()
 
-def _list_users(usr):
-    try:
-        with xmlrpc.client.ServerProxy(f"http://{addr}") as proxy:
-            return proxy.list_users(usr.current_room)
-    except Exception as e:
-        logger.error(f"{datetime.datetime.now()} at {_list_users.__name__}", exc_info=e)
-        messagebox.showerror(message=e)
-        exit()
+    def _list_users(self, usr):
+        try:
+            with xmlrpc.client.ServerProxy(f"http://{self.addr}") as proxy:
+                return proxy.list_users(usr.current_room)
+        except Exception as e:
+            logger.error(f"{datetime.datetime.now()} at {self._list_users.__name__}", exc_info=e)
+            messagebox.showerror(message=e)
+            exit()
 
 def main():
+    global handler
     logging.basicConfig(filename='myapp.log', level=logging.INFO)
     logger.info(f"{datetime.datetime.now()} Started at {main.__name__}")
     try:
-        global addr
-        with xmlrpc.client.ServerProxy(f"http://{host}:{port}") as proxy:
-            addr = proxy.lookup_procedure("messenger")
-    except Exception as e:
+        try:
+            with xmlrpc.client.ServerProxy(f"http://{HOST}:{PORT}") as proxy:
+                addr = proxy.lookup_procedure("messenger")
+        except Exception as e:
+            logger.error(f"{datetime.datetime.now()} at {main.__name__}", exc_info=e)
+        handler = Handler(addr)
+        app = App()
+    except KeyboardInterrupt as e:
         logger.error(f"{datetime.datetime.now()} at {main.__name__}", exc_info=e)
-        messagebox.showerror(message=e)
-        exit()
-    app = App()
     logger.info(f"{datetime.datetime.now()} Ended at {main.__name__}")
 
 if __name__ == "__main__":
-    with concurrent.futures.ProcessPoolExecutor(max_workers=4) as executor:
-        executor.submit(main)
+    main()
 
 # https://docs.python.org/3/library/xmlrpc.server.html
 # https://docs.python.org/3/library/xmlrpc.server.html#xmlrpc.server.SimpleXMLRPCServer
--- server.py
+++ server.py
@@ -2,24 +2,143 @@ from xmlrpc.server import SimpleXMLRPCServer
 import xmlrpc.client
 from socketserver import ThreadingMixIn
 from dataclasses import dataclass
-import concurrent.futures
 import time
 import datetime
 import logging
+import threading
 
+HOST = "localhost"
+PORT = 65432
 logger = logging.getLogger(__name__)
+messenger = None
 rooms = {}
-users = []
-host = "localhost"
-port = 65432
 
 class Server(ThreadingMixIn, SimpleXMLRPCServer):
     pass
 
+class Messenger():
+    def __init__(self):
+        global rooms
+        self.default_room = Room("default", ["everyone"], [], Timer())
+        rooms = {f"{self.default_room.name}":self.default_room}
+        self.users = []
+
+    def create_room(self, room_name):
+        if len(room_name) == 0:
+            return False
+        if room_name not in rooms:
+            room = Room(room_name, [], [], Timer())
+            rooms[room_name] = room
+            return True
+        return False
+
+    def join_room(self, username, room_name):
+        room = rooms[room_name]
+        if username not in room.users:
+            room.users.append(username)
+            msg = Message(
+                text=f"{username} joined.\n",
+                timestamp=datetime.datetime.now(),
+                sender='',
+                )
+            room.messages.append(msg)
+            return True
+        return False
+
+    def leave_room(self, username, room_name):
+        msg = Message(
+            text=f"{username} left.\n",
+            timestamp=datetime.datetime.now(),
+            sender='',
+            )
+        room = rooms[room_name]
+        room.messages.append(msg)
+        room.users.remove(username)
+        return True
+
+    def send_message(self, username, room_name, message, recipient=None):
+        if len(message) == 0:
+            return False
+        if recipient == username:
+            return False
+        room = rooms[room_name]
+        msg = Message(
+                text=message + '\n',
+                timestamp=datetime.datetime.now(),
+                sender=username,
+                )
+        if recipient:
+            msg.type = "unicast"
+            msg.addressee = recipient
+        room.messages.append(msg)
+        return True
+
+    def receive_messages(self, username, room_name):
+        room = rooms[room_name]
+        messages = room.messages
+        answer = []
+        for message in messages:
+            if message.type == "broadcast" or message.addressee == "everyone":
+                answer.append(f"[{message.timestamp.ctime()}]{message.sender}: {message.text}")
+            if message.addressee == username:
+                answer.append(f"[{message.timestamp.ctime()}]{message.sender} for you: {message.text}")
+        return answer
+
+    def list_rooms(self):
+        return list(rooms)
+
+    def list_users(self, room_name):
+        room = rooms[room_name]
+        return room.users
+
+    def register_user(self, username):
+        if len(username) < 0:
+            return False
+        if username not in self.users:
+            self.users.append(username)
+            rooms["default"].users.append(username)
+            return True
+        return False
+
+    def disconnect(self, username, room_name):
+        rooms[room_name].users.remove(username)
+        self.users.remove(username)
+        return True
+
+class TimerError(Exception):
+    pass
+
+class Timer:
+    def __init__(self):
+        self._start_time = None
+        self.buf = 0
+    
+    def start(self):
+        if self._start_time is not None:
+            raise TimerError(f"already running\n")
+        self._start_time = time.monotonic_ns()
+
+    def stop(self):
+        if self._start_time is None:
+            raise TimerError(f"not started")
+        self.buf = 0
+        elapsed_time = time.monotonic_ns() - self._start_time
+        self._start_time = None
+        return elapsed_time
+
+    def pause(self):
+        if self._start_time is None:
+            raise TimerError(f"not started (pause)")
+        elapsed_time = time.monotonic_ns() - self._start_time
+        self.buf += elapsed_time
+        return self.buf
+
 @dataclass
 class Room:
+    name: str
     users: list
     messages: list
+    timer: Timer
 
 @dataclass
 class Message:
@@ -29,126 +148,60 @@ class Message:
     type: str = "broadcast"
     addressee: str = None
 
-rooms["default"] = Room([],[])
+class RoomManager(threading.Thread):
+    def __init__(self):
+        global logger
+        global rooms
+        threading.Thread.__init__(self, daemon=True)
+    
+    def run(self):
+        while True:
+            r = list(rooms)
+            for room_name in r:
+                if room_name != "default":
+                    room = rooms[room_name]
+                    self.manage_room(room)
+            time.sleep(2)
+    
+    def manage_room(self, room: Room) -> None:
+        q = len(room.users)
+        t = room.timer._start_time
+        if t:
+            total = room.timer.pause() * 1e-9
+            if total > 300 and q == 0:
+                room.timer.stop()
+                rooms.pop(room.name)
+        if not t and q == 0:
+            room.timer.start()
+        if t and q > 0:
+            room.timer.stop()
 
-def create_room(room_name):
-    global rooms
-    if len(room_name) == 0:
-        return False
-    if room_name not in rooms:
-        room = Room([], [])
-        rooms[room_name] = room
-        return True
-    return False
-
-def join_room(username, room_name):
-    global rooms
-    room = rooms[room_name]
-    if username not in room.users:
-        room.users.append(username)
-        msg = Message(
-            text=f"{username} joined.\n",
-            timestamp=datetime.datetime.now(),
-            sender='',
-            )
-        room.messages.append(msg)
-        return True
-    return False
-
-def leave_room(username, room_name):
-    global rooms
-    room = rooms[room_name]
-    msg = Message(
-        text=f"{username} left.\n",
-        timestamp=datetime.datetime.now(),
-        sender='',
-        )
-    room.messages.append(msg)
-    room.users.remove(username)
-    return True
-
-def send_message(username, room_name, message, recipient=None):
-    global rooms
-    if len(message) == 0:
-        return False
-    room = rooms[room_name]
-    msg = Message(
-            text=message + '\n',
-            timestamp=datetime.datetime.now(),
-            sender=username,
-            )
-    if recipient == username:
-        return False
-    if recipient:
-        msg.type = "unicast"
-        msg.addressee = recipient
-    room.messages.append(msg)
-    return True
-
-def receive_messages(username, room_name):
-    global rooms
-    room = rooms[room_name]
-    messages = room.messages
-    answer = []
-    for message in messages:
-        if message.type == "broadcast":
-            answer.append(f"[{message.timestamp.ctime()}]{message.sender}: {message.text}")
-        if message.addressee == username:
-            answer.append(f"[{message.timestamp.ctime()}]{message.sender} for you: {message.text}")
-    return answer
-
-def list_rooms():
-    return list(rooms)
-
-def list_users(room_name):
-    global rooms
-    room = rooms[room_name]
-    return room.users
-
-def register_user(username):
-    global users
-    if len(username) < 0:
-        return False
-    if username not in users:
-        users.append(username)
-        rooms["default"].users.append(username)
-        return True
-    return False
-
-def disconnect(username, room_name):
-    global users
-    rooms[room_name].users.remove(username)
-    users.remove(username)
-    return True
+def task():
+    messenger = Messenger()
+    with Server((HOST, PORT)) as server:
+        server.register_introspection_functions()
+        server.register_instance(messenger)
+        try:
+            server.serve_forever()
+        except Exception as e:
+            logger.error(f"{datetime.datetime.now()} at serve_forever call", exc_info=e)
 
 def main():
     logging.basicConfig(filename='server.log', level=logging.INFO)
     logger.info(f"{datetime.datetime.now()} Started at {main.__name__}")
-
-    server = Server((host, port))
-    server.register_introspection_functions()
-    server.register_function(create_room)
-    server.register_function(join_room)
-    server.register_function(send_message)
-    server.register_function(receive_messages)
-    server.register_function(list_rooms)
-    server.register_function(list_users)
-    server.register_function(register_user)
-    server.register_function(disconnect)
-    server.register_function(leave_room)
-    
     try:
-        with xmlrpc.client.ServerProxy(f"http://{host}:{port - 1}") as proxy:
-            proxy.register_procedure("messenger", host, port)
-    except Exception as e:
+        try:
+            with xmlrpc.client.ServerProxy(f"http://{HOST}:{PORT - 1}") as proxy:
+                proxy.register_procedure("messenger", HOST, PORT)
+        except Exception as e:
+            logger.error(f"{datetime.datetime.now()} at {main.__name__}", exc_info=e)
+        s = threading.Thread(target=task)
+        s.start()
+        room_manager = RoomManager()
+        room_manager.start()
+        s.join()
+    except KeyboardInterrupt as e:
         logger.error(f"{datetime.datetime.now()} at {main.__name__}", exc_info=e)
-    
-    try:
-        server.serve_forever()
-    except KeyboardInterrupt as e: 
-        logger.error(f"{datetime.datetime.now()} at {main.__name__} serve_forever call", exc_info=e)
-    except Exception as e:
-        logger.error(f"{datetime.datetime.now()} at {main.__name__} serve_forever call", exc_info=e)
     logger.info(f"{datetime.datetime.now()} Ended at {main.__name__}")
 
 if __name__ == "__main__":
